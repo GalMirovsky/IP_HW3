8324e9f20afa6c0bfd2a5adb48ff5f003432e775
GalMirovsky <galmirovsky@gmail.com>
2021-12-04 19:52:51 +0200

    parameters added

--- hw3_script.py
+++ hw3_script.py
@@ -19,42 +19,43 @@ def main():
     median_radius = 2
     median_clean = cleanImageMedian(lena_sp_low, median_radius)
 
-    mean_radius = 5
-    mean_mask_std = 4
+    mean_radius = 2
+    mean_mask_std = 2
     mean_clean = cleanImageMean(lena_sp_low, mean_radius, mean_mask_std)
 
-    bi_radius = 3
+    bi_radius = 4
     bi_spatial_std = 30
-    bi_intensity_std = 80
+    bi_intensity_std = 45
     bi_f = bilateralFilt(lena_sp_low, bi_radius, bi_spatial_std, bi_intensity_std)
 
-    plot_results(lena_gray, lena_sp_low, noise_name, median_clean, mean_clean, bi_f)
+    # plot_results(lena_gray, lena_sp_low, noise_name, median_clean, mean_clean, bi_f)
+
     print(
         f"Conclusions for {noise_name} noise -----\n"
         f"1. Median because...\n"
-        f"2. \n"
-        f"3. \n"
+        f"2. Mean because...\n"
+        f"3. Bilateral because...\n"
     )  # TODO: add explanation
 
     # 2 ----------------------------------------------------------
     # add salt and pepper noise - high
     noise_name = 'salt and pepper - high'
-    high_SP_rate = 0.5
+    high_SP_rate = 0.4
     lena_sp_high = addSPnoise(lena_gray, high_SP_rate)
 
-    median_radius = 3
+    median_radius = 2
     median_clean = cleanImageMedian(lena_sp_high, median_radius)
 
-    mean_radius = 3
-    mean_mask_std = 1
+    mean_radius = 2
+    mean_mask_std = 2
     mean_clean = cleanImageMean(lena_sp_high, mean_radius, mean_mask_std)
 
-    bi_radius = 3
-    bi_spatial_std = 1
-    bi_intensity_std = 1
+    bi_radius = 4
+    bi_spatial_std = 30
+    bi_intensity_std = 45
     bi_f = bilateralFilt(lena_sp_high, bi_radius, bi_spatial_std, bi_intensity_std)
 
-    plot_results(lena_gray, lena_sp_high, noise_name, median_clean, mean_clean, bi_f)
+    # plot_results(lena_gray, lena_sp_high, noise_name, median_clean, mean_clean, bi_f)
 
     print(
         f"Conclusions for {noise_name} noise -----\n"
@@ -66,19 +67,19 @@ def main():
     # 3 ----------------------------------------------------------
     # add gaussian noise - low
     noise_name = 'gaussian noise - low'
-    low_gaussian_std = 30
+    low_gaussian_std = 20
     lena_gaussian = addGaussianNoise(lena_gray, low_gaussian_std)
 
-    median_radius = 3
+    median_radius = 2
     median_clean = cleanImageMedian(lena_gaussian, median_radius)
 
-    mean_radius = 3
-    mean_mask_std = 1
+    mean_radius = 2
+    mean_mask_std = 2
     mean_clean = cleanImageMean(lena_gaussian, mean_radius, mean_mask_std)
 
-    bi_radius = 3
-    bi_spatial_std = 1
-    bi_intensity_std = 1
+    bi_radius = 4
+    bi_spatial_std = 15
+    bi_intensity_std = 30
     bi_f = bilateralFilt(lena_gaussian, bi_radius, bi_spatial_std, bi_intensity_std)
 
     plot_results(lena_gray, lena_gaussian, noise_name, median_clean, mean_clean, bi_f)
@@ -93,19 +94,19 @@ def main():
     # 4 ----------------------------------------------------------
     # add gaussian noise - high
     noise_name = 'gaussian noise - high'
-    high_gaussian_std = 60
+    high_gaussian_std = 55
     lena_gaussian = addGaussianNoise(lena_gray, high_gaussian_std)
 
-    median_radius = 3
+    median_radius = 2
     median_clean = cleanImageMedian(lena_gaussian, median_radius)
 
-    mean_radius = 3
-    mean_mask_std = 1
+    mean_radius = 2
+    mean_mask_std = 2
     mean_clean = cleanImageMean(lena_gaussian, mean_radius, mean_mask_std)
 
-    bi_radius = 3
-    bi_spatial_std = 1
-    bi_intensity_std = 1
+    bi_radius = 4
+    bi_spatial_std = 30
+    bi_intensity_std = 45
     bi_f = bilateralFilt(lena_gaussian, bi_radius, bi_spatial_std, bi_intensity_std)
 
     plot_results(lena_gray, lena_gaussian, noise_name, median_clean, mean_clean, bi_f)
@@ -137,6 +138,7 @@ def plot_results(original, noisy, noise_name, median_clean, mean_clean, bi_f):
     plt.subplot(2, 3, 6)
     plt.imshow(bi_f, cmap='gray', vmin=0, vmax=255)
     plt.title("bilateral")
+    plt.show()
 
 
 if __name__ == '__main__':
